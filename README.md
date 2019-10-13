8f5a1e2bf606fb1b3a5e863697fb87eac93ec0af
Alexey Galitsyn <scorpionls@mail.ru>
2019-10-13 03:47:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ImGui-OSG
+This application shows how to use OpenSceneGraph and Dear ImGui together.
+
+## Prerequisites
+This example requires:
+ - OpenSceneGraph
+ - GLEW
+ - OpenGL
+
+You may install them using vcpkg on Windows or apt on Ubuntu.
+
+## How to build
+```
+mkdir build && cd build
+cmake ..
+cmake --build .
+```
---
+++ cmake/CreateImGuiTarget.cmake
@@ -0,0 +1,105 @@
+# Downloads and creates target for Dear ImGui
+#
+# Dear ImGui is not a cmake project (yet?) so we have to manually
+# add it's sources and compile it within our project.
+#
+# Arguments:
+#   REPOSITORY     - git repository to download sources from
+#   TAG            - git tag or branch
+#   LOADER         - OpenGL extension loader, possible values: glew, gl3w
+#   IMPLEMENTATION - ImGui implementation, check imgui examples dir
+#
+# Result:
+#   Creates target imgui::imgui
+include(CMakeFindDependencyMacro)
+include(FetchContent)
+function(CreateImGuiTarget)
+    # Define the supported set of keywords
+    set(prefix imgui)
+    set(noValues)
+    set(singleValues LOADER IMPLEMENTATION REPOSITORY TAG)
+    set(multiValues)
+
+    # Process the arguments passed in
+    include(CMakeParseArguments)
+    cmake_parse_arguments(${prefix}
+        "${noValues}"
+        "${singleValues}"
+        "${multiValues}"
+        ${ARGN}
+    )
+
+    message(STATUS "ImGui LOADER = " ${imgui_LOADER})
+    message(STATUS "ImGui IMPLEMENTATION = " ${imgui_IMPLEMENTATION})
+    message(STATUS "ImGui REPOSITORY = " ${imgui_REPOSITORY})
+    message(STATUS "ImGui TAG = " ${imgui_TAG})
+
+    if(NOT imgui_REPOSITORY OR NOT imgui_TAG)
+        message(FATAL_ERROR "Please, specify imgui repository and tag")
+    endif()
+
+    string(TOLOWER "${imgui_LOADER}" imgui_LOADER_lower )
+    string(TOLOWER "${imgui_IMPLEMENTATION}" imgui_IMPLEMENTATION_lower )
+
+    FetchContent_Declare(imgui
+        GIT_REPOSITORY "${imgui_REPOSITORY}"
+        GIT_TAG "${imgui_TAG}"
+        GIT_SHALLOW YES
+    )
+
+    FetchContent_GetProperties(imgui)
+
+    if(NOT imgui_POPULATED)
+        FetchContent_Populate(imgui)
+    endif()
+
+    # Core ImGui stuff:
+    add_library(imgui INTERFACE)
+    add_library(imgui::imgui ALIAS imgui)
+    target_sources(imgui INTERFACE
+        ${imgui_SOURCE_DIR}/imgui.cpp
+        ${imgui_SOURCE_DIR}/imgui_demo.cpp
+        ${imgui_SOURCE_DIR}/imgui_draw.cpp
+        ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+    )
+    target_include_directories(imgui INTERFACE
+        ${imgui_SOURCE_DIR}
+        ${imgui_SOURCE_DIR}/examples
+    )
+
+    # OpenGL loader
+    if(imgui_LOADER_lower STREQUAL "glew")
+        find_dependency(GLEW)
+        target_link_libraries(imgui
+            INTERFACE
+                GLEW::GLEW
+        )
+        target_compile_definitions(imgui
+            INTERFACE
+                IMGUI_IMPL_OPENGL_LOADER_GLEW
+        )
+    elseif(imgui_LOADER_lower STREQUAL "gl3w")
+        target_compile_definitions(imgui
+            INTERFACE
+                IMGUI_IMPL_OPENGL_LOADER_GL3W
+        )
+    else()
+        message(FATAL_ERROR "Unknown ImGui loader ${imgui_LOADER}")
+    endif()
+
+    # Implementation
+    if(imgui_IMPLEMENTATION_lower STREQUAL "opengl3")
+        find_dependency(OpenGL)
+        target_link_libraries(imgui
+            INTERFACE
+                OpenGL::GL
+        )
+        target_sources(imgui
+            INTERFACE
+                ${imgui_SOURCE_DIR}/examples/imgui_impl_opengl3.h
+                ${imgui_SOURCE_DIR}/examples/imgui_impl_opengl3.cpp
+        )
+    else()
+        message(FATAL_ERROR "Unknown ImGui implementation ${imgui_IMPLEMENTATION}")
+    endif()
+endfunction()
